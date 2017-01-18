6f1ae16101ff9192f340ef43d897ce2bb81a965d
Pirvali, Behzad <bpirvali@gmail.com>
2017-01-17 22:32:07 -0800

    added crypto fundamentals

---
+++ cryptography-fundamentals-java-dotnet-developers/3-cryptography-fundamentals-java-dotnet-developers-m3-apis-exercise-files/After/DotNet/EncryptionTests/AsymmetricTests.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Security.Cryptography;
+using FluentAssertions;
+using System.Linq;
+using System.IO;
+
+namespace EncryptionTests
+{
+    [TestClass]
+    public class AsymmetricTests
+    {
+        [TestMethod]
+        public void GenerateAnRsaKeyPair()
+        {
+            AsymmetricAlgorithm rsa = new RSACryptoServiceProvider(2048);
+
+            string encodedKey = rsa.ToXmlString(true);
+
+            encodedKey.Should().Contain("<Modulus>");
+            encodedKey.Should().Contain("<Exponent>AQAB</Exponent>");
+            encodedKey.Should().Contain("<D>");
+        }
+
+        [TestMethod]
+        public void ShareAPublicKey()
+        {
+            AsymmetricAlgorithm rsa = new RSACryptoServiceProvider(2048);
+
+            string encodedKey = rsa.ToXmlString(false);
+
+            encodedKey.Should().Contain("<Modulus>");
+            encodedKey.Should().Contain("<Exponent>AQAB</Exponent>");
+            encodedKey.Should().NotContain("<D>");
+        }
+
+        [TestMethod]
+        public void ExponentIsAlways65537()
+        {
+            byte[] exponent = Convert.FromBase64String("AQAB");
+
+            exponent.Length.Should().Be(3);
+            long number =
+                ((long)exponent[2] << 16) +
+                ((long)exponent[1] << 8) +
+                ((long)exponent[0]);
+            number.Should().Be(65537);
+        }
+
+        [TestMethod]
+        public void EncryptASymmetricKey()
+        {
+            var rsa = new RSACryptoServiceProvider(2048);
+
+            byte[] blob = rsa.ExportCspBlob(false);
+            var publicKey = new RSACryptoServiceProvider();
+            publicKey.ImportCspBlob(blob);
+
+            var aes = new AesCryptoServiceProvider();
+            aes.KeySize = 256;
+            byte[] encryptedKey = publicKey.Encrypt(aes.Key, true);
+            byte[] decryptedKey = rsa.Decrypt(encryptedKey, true);
+
+            Enumerable.SequenceEqual(
+                decryptedKey,
+                aes.Key)
+                .Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void SignAMessage()
+        {
+            var rsa = new RSACryptoServiceProvider(2048);
+
+            byte[] blob = rsa.ExportCspBlob(false);
+            var publicKey = new RSACryptoServiceProvider();
+            publicKey.ImportCspBlob(blob);
+
+            string message = "Alice knows Bob's secret.";
+            var memory = new MemoryStream();
+            using (var writer = new StreamWriter(memory))
+            {
+                writer.Write(message);
+            }
+
+            var hashFunction = new SHA256CryptoServiceProvider();
+            byte[] signature = rsa.SignData(memory.ToArray(), hashFunction);
+
+            bool verified = publicKey.VerifyData(
+                memory.ToArray(), hashFunction, signature);
+            verified.Should().BeTrue();
+        }
+    }
+}
---
+++ cryptography-fundamentals-java-dotnet-developers/3-cryptography-fundamentals-java-dotnet-developers-m3-apis-exercise-files/After/Java/CryptoFundamentalsTests/src/cryptofundamentals/tests/AsymmetricTests.java
@@ -0,0 +1,107 @@
+package cryptofundamentals.tests;
+
+import static org.junit.Assert.*;
+
+import java.security.InvalidKeyException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.Signature;
+import java.security.SignatureException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+
+import org.junit.Test;
+
+public class AsymmetricTests {
+
+	@Test
+	public void generateAnRsaKeyPair() throws Exception {
+		KeyPair keyPair = generateRsaKey();
+		
+		assertEquals("RSA", keyPair.getPublic().getAlgorithm());
+		assertTrue(keyPair.getPublic().getEncoded().length > 2048 / 8);
+		assertTrue(keyPair.getPrivate().getEncoded().length > 2048 / 8);
+	}
+	
+	@Test
+	public void encryptASymmetricKey() throws Exception {
+		KeyPair keyPair = generateRsaKey();
+
+		PublicKey publicKey = keyPair.getPublic();
+		PrivateKey privateKey = keyPair.getPrivate();
+		
+		KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+		keyGenerator.init(256);
+		SecretKey key = keyGenerator.generateKey();
+		
+		byte[] encryptedKey = encryptWithRsa(publicKey, key);
+		byte[] decryptedKey = decryptWithRsa(privateKey, encryptedKey);
+		
+		assertArrayEquals(key.getEncoded(), decryptedKey);
+	}
+	
+	@Test
+	public void signAMessage() throws Exception {
+		KeyPair keyPair = generateRsaKey();
+
+		PublicKey publicKey = keyPair.getPublic();
+		PrivateKey privateKey = keyPair.getPrivate();
+
+		String message = "Alice knows Bob's secret.";
+		byte[] messageBytes = message.getBytes();
+		
+		byte[] signatureBytes = signMessage(privateKey, messageBytes);
+		boolean verified = verifySignature(publicKey, messageBytes, signatureBytes);
+		
+		assertTrue(verified);
+	}
+
+	private KeyPair generateRsaKey() throws NoSuchAlgorithmException {
+		KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+		generator.initialize(2048);
+		return generator.generateKeyPair();
+	}
+
+	private byte[] encryptWithRsa(PublicKey publicKey, SecretKey key)
+			throws Exception {
+		
+		Cipher rsa = Cipher.getInstance("RSA/ECB/OAEPWithSHA-256AndMGF1Padding");
+		rsa.init(Cipher.ENCRYPT_MODE, publicKey);
+		return rsa.doFinal(key.getEncoded());
+	}
+
+	private byte[] decryptWithRsa(PrivateKey privateKey, byte[] encryptedKey)
+			throws Exception {
+		
+		Cipher rsa = Cipher.getInstance("RSA/ECB/OAEPWithSHA-256AndMGF1Padding");
+		rsa.init(Cipher.DECRYPT_MODE, privateKey);
+		return rsa.doFinal(encryptedKey);
+	}
+
+	private byte[] signMessage(PrivateKey privateKey, byte[] messageBytes)
+			throws Exception {
+		
+		Signature signature = Signature.getInstance("SHA256withRSA");
+		signature.initSign(privateKey);
+		signature.update(messageBytes);
+		return signature.sign();
+	}
+
+	private boolean verifySignature(PublicKey publicKey, byte[] messageBytes,
+			byte[] signatureBytes) throws Exception {
+		
+		Signature signature = Signature.getInstance("SHA256withRSA");
+		signature.initVerify(publicKey);
+		signature.update(messageBytes);
+		return signature.verify(signatureBytes);
+	}
+
+}
